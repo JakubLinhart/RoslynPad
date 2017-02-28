85a6a1baaa1dc6c32d975c896d75c4277c0a89cb
Jakub Linhart <jakub@postsharp.net>
2017-02-28 10:06:20 +0100

    Renaming.

--- src/RoslynPad.Common.UI/CSharpScriptEngine.cs
+++ src/RoslynPad.Common.UI/CSharpScriptEngine.cs
@@ -21,10 +21,10 @@ namespace RoslynPad.UI
             "using System.Threading;",
             "using Infusion.Proxy;",
             "using Infusion.Packets;",
-            "using Infusion.Proxy.InjectionApi;",
+            "using Infusion.Proxy.LegacyApi;",
             "using Infusion.Packets.Parsers;",
             "using Infusion.Gumps;",
-            "using static Infusion.Proxy.InjectionApi.Injection;"
+            "using static Infusion.Proxy.LegacyApi.Injection;"
         };
 
         private readonly IScriptOutput scriptOutput;
